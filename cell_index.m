6e5f8c15af62d28d63b0869b9d21d8522f93ef63
TamerGezici <tamergezici@gmail.com>
2022-11-22 11:38:32 +0300

    Replace cell_index.m with newer version from AA

--- cell_index.m
+++ cell_index.m
@@ -1,18 +1,20 @@
-function i = cell_index(lines, str, nl, ni)
-if nargin < 3
-    nl = 1;
+% Locate string "str" within a cell of strings "lines"
+% Returns 0 if none
+% Optional parameters:
+% 	nl:	starting index (default = 1)
+% 	ni:	returns location only if "str" is a substring starting from "ni"th character (default = none --> any match)
+% Tibor Auer MRC CBU Cambridge 2012-2013
+
+function r = cell_index(varargin)
+nl = 1;
+varargin{1}(cellfun(@(x) ~ischar(x), varargin{1})) = {''}; % ingore nonchar elements
+
+if (nargin >= 3) && ~isempty(varargin{3}), nl = varargin{3}; end
+if (nargin >= 4)
+    r = find(cellfun(@(x) ~isempty(x) && (x == varargin{4}),strfind(varargin{1}(nl:end),varargin{2})));
+else
+    r = find(cellfun(@(x) ~isempty(x),strfind(varargin{1}(nl:end),varargin{2})));
 end
-for i = nl:numel(lines)
-    test = lines{i};
-    if iscell(test) || (size(test,1) > 1)
-        test = [];
-        continue; 
-    end
-    if size(test,1) > size(test,2), test = test';  end
-    if ~ischar(test), test = num2str(test); end
-    test = strfind(test,str);
-    if ~isempty(test) && ((nargin < 4) || ((nargin > 3) && any(test == ni))), break, end
-end
-if isempty(test)
-    i = 0;
+if isempty(r), r = 0; end
+r = r';
 end
